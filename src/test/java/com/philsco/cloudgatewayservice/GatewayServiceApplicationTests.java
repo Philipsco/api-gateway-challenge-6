64501d6279392bec44fc5fd5c897f060fb967996
Philip Bagus Kristianto <bagus09051998@gmail.com>
2022-12-11 07:45:30 +0700

    api gateway challenge 7

---
+++ src/test/java/com/philsco/cloudgatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philsco.cloudgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
